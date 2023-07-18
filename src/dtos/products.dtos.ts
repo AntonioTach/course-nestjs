6b37939714fedf71e2434209ec786ed0fe4c186c
AntonioTach <antach.vihe@gmail.com>
2023-07-17 21:37:48 -0600

    Validations with validator, mapped e incorrect params

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,9 +1,29 @@
+import { IsString, IsNumber, IsUrl, IsNotEmpty, IsPositive } from "class-validator";
+import { PartialType } from "@nestjs/mapped-types"
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty({ message: "Name Product cannot be empty" })
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
+
 }
 
 export class UpdateProductDto {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    //prohibir datos que se ingresan y no estan en el dto.
+    whitelist: true, //Ignrorar
+    forbidNonWhitelisted: true, //Alertar
+  }));
   await app.listen(3000);
 }
 bootstrap();
